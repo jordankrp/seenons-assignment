79e4597820417d25df0127e5237b71e0a76e13c1
Jordan Karapanagiotis <jordan.krp.93@gmail.com>
2022-07-23 10:32:31 +0200

    Get wastes streams from seensons API

---
+++ integration.py
@@ -0,0 +1,17 @@
+import requests
+import argparse
+
+BASE = 'https://api-dev-593.seenons.com/api/me/streams'
+QUERY_KEY = 'postal_code'
+
+def get_waste_streams(post_code):
+    url = f'{BASE}?{QUERY_KEY}={post_code}'
+    waste_streams = requests.get(url).json()
+    print(waste_streams)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Post code parser')
+    parser.add_argument('-p', '--post-code', help='Post code (4 integers only)', required=True)
+    args = parser.parse_args()
+
+    get_waste_streams(args.post_code)
