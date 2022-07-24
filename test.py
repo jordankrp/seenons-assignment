37af8b50cd88ea4c347aea6286fd2336058230d4
Jordan Karapanagiotis <jordan.krp.93@gmail.com>
2022-07-24 10:50:14 +0200

    Added unit tests for house number

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import unittest
-from integration import get_bagid
+from integration import get_bagid, get_house_info
 
 
 class TestIntegration(unittest.TestCase):
     def test_1(self):
-        # Test if we get correct bag ID
+        # Test bag ID for correct address
         post_code = "2512HE"
         house_number = "68"
         house_letter = "A"
@@ -13,8 +13,32 @@ class TestIntegration(unittest.TestCase):
         self.assertEqual(bag_id, expected_bag_id)
         print("Test 1 completed")
 
+    def test_2(self):
+        # Test correct house number
+        post_code = "2512HE"
+        house_number = "66"
+        house_info = get_house_info(post_code, house_number)
+        # House info expected to be a list of dict
+        self.assertNotEqual(house_info, [])
+        # House letter should be a dict key
+        for item in house_info:
+            self.assertIn('huisletter', item)
+        print("Test 2 completed")
+
+    def test_3(self):
+        # Test wrong house number
+        post_code = "2514HE"
+        house_number = "34"
+        house_info = get_house_info(post_code, house_number)
+        # House info expected to be an empty list
+        expected_house_info = []
+        self.assertEqual(house_info, expected_house_info)
+        print("Test 3 completed")
+
 
 if __name__ == "__main__":
     tester = TestIntegration()
 
     tester.test_1()
+    tester.test_2()
+    tester.test_3()
